3df8687ae3c45a2a7ba183727fb2938d325d3e54
Luan Coêlho <lumyth.br@gmail.com>
2023-09-13 01:29:42 -0300

    feat: Criando script para copilar latex para pdf.

---
+++ src/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Limpa e cria a pasta 'out'
+rm -rf out
+mkdir out
+
+# Executa os comandos LaTeX
+pdflatex -output-directory=out main.tex
+bibtex out/main.aux
+pdflatex -output-directory=out main.tex
+pdflatex -output-directory=out main.tex
+
+# Move a pasta 'out' para a raiz do projeto
+mv out ../
